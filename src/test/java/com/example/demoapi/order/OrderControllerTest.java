2f14b3f0cb4b285e985175a4fa650e5a4653711b
Marcin Lewandowski <marcinlewandowski1999@gmail.com>
2024-07-26 22:22:42 +0200

    Add tests for POST, PUT, DELETE requests

--- src/test/java/com/example/demoapi/order/OrderControllerTest.java
+++ src/test/java/com/example/demoapi/order/OrderControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demoapi.order;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -19,8 +20,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(OrderController.class)
@@ -38,53 +38,60 @@ public class OrderControllerTest {
     @MockBean
     private OrderRepository repository;
 
-    private Order mockOrder1;
-    private Order mockOrder2;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    private Order mockOrderInProgress;
+    private Order mockOrderCompleted;
+    private Order updatedMockOrderInProgress;
     private static final MediaType HAL_JSON = new MediaType("application", "hal+json");
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
 
-        mockOrder1 = new Order(1L, Status.IN_PROGRESS, "Meat");
-        mockOrder2 = new Order(2L, Status.COMPLETED, "Beans");
-        List<Order> mockOrders = List.of(mockOrder1, mockOrder2);
+        mockOrderInProgress = new Order(1L, Status.IN_PROGRESS, "Meat");
+        mockOrderCompleted = new Order(2L, Status.COMPLETED, "Beans");
+        updatedMockOrderInProgress = new Order(1L, Status.IN_PROGRESS, "Orange");
+        List<Order> mockOrders = List.of(mockOrderInProgress, mockOrderCompleted);
 
+        // orderService methods
         when(orderService.getOrders()).thenReturn(mockOrders);
-        when(orderService.getOrderById(1L)).thenReturn(mockOrder1);
-        when(orderService.getOrderById(2L)).thenReturn(mockOrder2);
+        when(orderService.getOrderById(1L)).thenReturn(mockOrderInProgress);
+        when(orderService.getOrderById(2L)).thenReturn(mockOrderCompleted);
+        when(orderService.saveOrder(mockOrderInProgress)).thenReturn(mockOrderInProgress);
+        when(orderService.updateOrder(mockOrderInProgress, 2L)).thenReturn(mockOrderInProgress);
 
         // Mocked HAL representation for mockOrder1
-        when(orderEntityModelAssembler.toModel(mockOrder1))
-                .thenReturn(EntityModel.of(mockOrder1)
-                        .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrder1.getId())).withSelfRel())
+        when(orderEntityModelAssembler.toModel(mockOrderInProgress))
+                .thenReturn(EntityModel.of(mockOrderInProgress)
+                        .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrderInProgress.getId())).withSelfRel())
                         .add(linkTo(methodOn(OrderController.class).getOrders()).withRel("orders"))
-                        .add(linkTo(methodOn(OrderController.class).cancelOrder(mockOrder1.getId())).withRel("cancel"))
-                        .add(linkTo(methodOn(OrderController.class).completeOrder(mockOrder1.getId())).withRel("complete")));
+                        .add(linkTo(methodOn(OrderController.class).cancelOrder(mockOrderInProgress.getId())).withRel("cancel"))
+                        .add(linkTo(methodOn(OrderController.class).completeOrder(mockOrderInProgress.getId())).withRel("complete")));
+
 
         // Mocked HAL representation for mockOrder2
-        when(orderEntityModelAssembler.toModel(mockOrder2))
-                .thenReturn(EntityModel.of(mockOrder2)
-                        .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrder2.getId())).withSelfRel())
+        when(orderEntityModelAssembler.toModel(mockOrderCompleted))
+                .thenReturn(EntityModel.of(mockOrderCompleted)
+                        .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrderCompleted.getId())).withSelfRel())
                         .add(linkTo(methodOn(OrderController.class).getOrders()).withRel("orders")));
 
         // Mocked HAL representation for collection of orders
         when(orderEntityModelAssembler.toCollectionModel(mockOrders))
                 .thenReturn(CollectionModel.of(List.of(
-                        EntityModel.of(mockOrder1)
-                                .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrder1.getId())).withSelfRel())
+                        EntityModel.of(mockOrderInProgress)
+                                .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrderInProgress.getId())).withSelfRel())
                                 .add(linkTo(methodOn(OrderController.class).getOrders()).withRel("orders"))
-                                .add(linkTo(methodOn(OrderController.class).cancelOrder(mockOrder1.getId())).withRel("cancel"))
-                                .add(linkTo(methodOn(OrderController.class).completeOrder(mockOrder1.getId())).withRel("complete")),
-                        EntityModel.of(mockOrder2)
-                                .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrder2.getId())).withSelfRel())
+                                .add(linkTo(methodOn(OrderController.class).cancelOrder(mockOrderInProgress.getId())).withRel("cancel"))
+                                .add(linkTo(methodOn(OrderController.class).completeOrder(mockOrderInProgress.getId())).withRel("complete")),
+                        EntityModel.of(mockOrderCompleted)
+                                .add(linkTo(methodOn(OrderController.class).getOrderById(mockOrderCompleted.getId())).withSelfRel())
                                 .add(linkTo(methodOn(OrderController.class).getOrders()).withRel("orders"))
                 )));
     }
 
     @Test
     public void givenOrdersExist_whenGetOrders_thenReturnOrderListWithLinks() throws Exception {
-
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/" + API_VERSION + "/orders")
                         .accept(HAL_JSON))
@@ -92,33 +99,87 @@ public class OrderControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$._embedded.orderList").exists())
                 .andExpect(jsonPath("$._embedded.orderList[*].id").isNotEmpty())
-                .andExpect(jsonPath("$._embedded.orderList[0].id").value(mockOrder1.getId()))
-                .andExpect(jsonPath("$._embedded.orderList[0].status").value(mockOrder1.getStatus().toString()))
-                .andExpect(jsonPath("$._embedded.orderList[0].description").value(mockOrder1.getDescription()))
+                .andExpect(jsonPath("$._embedded.orderList[0].id").value(mockOrderInProgress.getId()))
+                .andExpect(jsonPath("$._embedded.orderList[0].status").value(mockOrderInProgress.getStatus().toString()))
+                .andExpect(jsonPath("$._embedded.orderList[0].description").value(mockOrderInProgress.getDescription()))
                 .andExpect(jsonPath("$._embedded.orderList[0]._links.self.href").value("/" + API_VERSION + "/orders/1"))
+                .andExpect(jsonPath("$._embedded.orderList[0]._links.orders.href").value("/" + API_VERSION + "/orders"))
                 .andExpect(jsonPath("$._embedded.orderList[0]._links.cancel.href").value("/" + API_VERSION + "/orders/1/cancel"))
                 .andExpect(jsonPath("$._embedded.orderList[0]._links.complete.href").value("/" + API_VERSION + "/orders/1/complete"))
-                .andExpect(jsonPath("$._embedded.orderList[1].id").value(mockOrder2.getId()))
-                .andExpect(jsonPath("$._embedded.orderList[1].status").value(mockOrder2.getStatus().toString()))
-                .andExpect(jsonPath("$._embedded.orderList[1].description").value(mockOrder2.getDescription()))
+                .andExpect(jsonPath("$._embedded.orderList[1].id").value(mockOrderCompleted.getId()))
+                .andExpect(jsonPath("$._embedded.orderList[1].status").value(mockOrderCompleted.getStatus().toString()))
+                .andExpect(jsonPath("$._embedded.orderList[1].description").value(mockOrderCompleted.getDescription()))
                 .andExpect(jsonPath("$._embedded.orderList[1]._links.self.href").value("/" + API_VERSION + "/orders/2"))
                 .andExpect(jsonPath("$._embedded.orderList[1]._links.orders.href").value("/" + API_VERSION + "/orders"));
     }
 
     @Test
-    public void givenOrder1Exist_whenGetOrderById_thenReturnOrderWithLinks() throws Exception {
+    public void givenOrderExists_whenGetOrderById_thenReturnOrderWithLinks() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/" + API_VERSION + "/orders/1")
                         .accept(HAL_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").exists())
-                .andExpect(jsonPath("$.id").value(mockOrder1.getId()))
-                .andExpect(jsonPath("$.status").value(mockOrder1.getStatus().toString()))
-                .andExpect(jsonPath("$.description").value(mockOrder1.getDescription()))
+                .andExpect(jsonPath("$.id").value(mockOrderInProgress.getId()))
+                .andExpect(jsonPath("$.status").value(mockOrderInProgress.getStatus().toString()))
+                .andExpect(jsonPath("$.description").value(mockOrderInProgress.getDescription()))
                 .andExpect(jsonPath("$._links.self.href").value("/" + API_VERSION + "/orders/1"))
+                .andExpect(jsonPath("$._links.orders.href").value("/" + API_VERSION + "/orders"))
                 .andExpect(jsonPath("$._links.cancel.href").value("/" + API_VERSION + "/orders/1/cancel"))
                 .andExpect(jsonPath("$._links.complete.href").value("/" + API_VERSION + "/orders/1/complete"));
     }
+
+    @Test
+    public void givenNewOrder_whenPostOrder_thenReturnSavedOrderWithLinks() throws Exception {
+        String jsonOrder = objectMapper.writeValueAsString(mockOrderInProgress);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/" + API_VERSION + "/orders")
+                        .contentType(HAL_JSON).content(jsonOrder)
+                        .accept(HAL_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", "/" + API_VERSION + "/orders/1"))
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id").value(mockOrderInProgress.getId()))
+                .andExpect(jsonPath("$.status").value(mockOrderInProgress.getStatus().toString()))
+                .andExpect(jsonPath("$.description").value(mockOrderInProgress.getDescription()))
+                .andExpect(jsonPath("$._links.self.href").value("/" + API_VERSION + "/orders/1"))
+                .andExpect(jsonPath("$._links.orders.href").value("/" + API_VERSION + "/orders"))
+                .andExpect(jsonPath("$._links.cancel.href").value("/" + API_VERSION + "/orders/1/cancel"))
+                .andExpect(jsonPath("$._links.complete.href").value("/" + API_VERSION + "/orders/1/complete"));
+    }
+
+    @Test
+    public void givenOrderExists_whenPutOrderWithNewParameters_thenReturnUpdatedOrderWithLinks() throws Exception {
+        String jsonOrder = objectMapper.writeValueAsString(mockOrderInProgress);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put(("/" + API_VERSION + "/orders/2"), mockOrderInProgress)
+                        .content(jsonOrder)
+                        .contentType(HAL_JSON)
+                        .accept(HAL_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", "/" + API_VERSION + "/orders/1"))
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.id").value(mockOrderInProgress.getId()))
+                .andExpect(jsonPath("$.status").value(mockOrderInProgress.getStatus().toString()))
+                .andExpect(jsonPath("$.description").value(mockOrderInProgress.getDescription()))
+                .andExpect(jsonPath("$._links.self.href").value("/" + API_VERSION + "/orders/1"))
+                .andExpect(jsonPath("$._links.orders.href").value("/" + API_VERSION + "/orders"))
+                .andExpect(jsonPath("$._links.cancel.href").value("/" + API_VERSION + "/orders/1/cancel"))
+                .andExpect(jsonPath("$._links.complete.href").value("/" + API_VERSION + "/orders/1/complete"));
+    }
+
+    @Test
+    public void givenOrder_whenDeleted_thenReturnNoContent() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/" + API_VERSION + "/orders/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
 
 }
