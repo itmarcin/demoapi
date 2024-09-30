0e305f9c6dc8080d6170742553ba55810cb1a968
Marcin Lewandowski <marcinlewandowski1999@gmail.com>
2024-09-30 11:19:17 +0200

    Test api

--- src/main/java/com/example/demoapi/DemoapiApplication.java
+++ src/main/java/com/example/demoapi/DemoapiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoapiApplication {
 
 	public static final String API_VERSION = "api/v1";
-	public String removeMeLater = "tmp";
 	public static void main(String[] args) {
 		SpringApplication.run(DemoapiApplication.class, args);
 	}
